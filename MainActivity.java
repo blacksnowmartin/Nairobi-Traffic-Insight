23c522a4c877bef5cf02710a16d53f3ff5f17111
Martin Kitonga <119602263+blacksnowmartin@users.noreply.github.com>
2023-10-26 19:07:17 +0300

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,30 @@
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class TrafficMapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_traffic_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapView);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Customize the map here, e.g., set markers, polygons, or overlays
+    }
+}
